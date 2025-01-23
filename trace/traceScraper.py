31a349c441d68e8c11b671b274f3e641215abfa1
Bob Zheng <bob.zheng6042@gmail.com>
2025-01-23 15:34:47 -0500

    starting trace

---
+++ trace/traceScraper.py
@@ -0,0 +1,20 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+import re
+
+def main():
+
+    cookies = {'JSESSIONID': '1DB2489F856C5A11EAC41DF269A77924'}
+
+    response = requests.post("https://www.applyweb.com/eval/new/student#/home")
+
+    print(response.text)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    # print(soup.prettify())
+
+
+if __name__ == "__main__":
+    main()
